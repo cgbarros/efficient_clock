79d9417576e944052084b005ec0853d554a3c3e8
Caio Barros <caio.barros@gmail.com>
2024-02-27 16:29:05 -0300

    finished drawing the clock and blinking the separator

--- clock.ino
+++ clock.ino
@@ -31,8 +31,8 @@ void hour_top_i(uint16_t color) {
 }
 
 void separator(uint16_t color) {
-  oled.fillCircle(43, 22, 2, color);
-  oled.fillCircle(43, 42, 2, color);
+  oled.fillCircle(46, 22, 2, color);
+  oled.fillCircle(46, 42, 2, color);
 }
 
 void min_dec_bottom(uint16_t color) {
@@ -48,6 +48,26 @@ void min_dec_left_i(uint16_t color) {
 
 void min_dec_right_i(uint16_t color) {
   oled.drawLine(74, 16, 74, 32, color);
+}
+
+void min_uni_v (uint16_t color) {
+  oled.drawLine(96, 22, 108, 54, color);
+  oled.drawLine(108, 54, 120, 22, color);
+}
+
+void min_uni_top (uint16_t color) {
+  oled.drawLine(94, 10, 122, 10, color);
+  oled.drawLine(94, 10, 94, 18, color);
+  oled.drawLine(108, 10, 108, 15, color);
+  oled.drawLine(122, 10, 122, 18, color);
+}
+
+void min_uni_left (uint16_t color) {
+  oled.drawLine(92, 30, 92, 46, color);
+}
+
+void min_uni_right (uint16_t color) {
+  oled.drawLine(124, 30, 124, 46, color);
 }
 
 void setup() {
@@ -73,16 +93,30 @@ void setup() {
   min_dec_bottom(WHITE);
   min_dec_left_i(WHITE);
   min_dec_right_i(WHITE);
+  
+  min_uni_v(WHITE);
+  min_uni_top(WHITE);
+  min_uni_left(WHITE);
+  min_uni_right(WHITE);
 
   
+  
+
 
   // oled.setTextSize(1);         // set text size
   // oled.setTextColor(WHITE);    // set text color
   // oled.setCursor(0, 2);       // set position to display (x,y)
-  // oled.println("cgbarros"); // set text
+  // oled.println("Robotronix"); // set text
   oled.display();              // display on OLED
 }
 
 void loop() {
-}
 
+  delay(500);
+  separator(BLACK);
+  oled.display();
+  delay(500);
+  separator(WHITE);
+  oled.display();
+
+}
